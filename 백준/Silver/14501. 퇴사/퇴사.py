deeeda4a5c83054d9b2addc8ed5d23fbaca0eb4c
Noh dongwon <102791306+Kor-NOH@users.noreply.github.com>
2024-04-03 18:25:34 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+schedule_list = []
+dp = [0] * (n+1)
+
+for _ in range(n):
+    t, p = map(int, sys.stdin.readline().split())
+    schedule_list.append([t, p])
+
+for i in range(n+1):
+    max_num = 0
+    for j in range(0, i):
+        if schedule_list[j][0] <= n-j:
+            if schedule_list[j][0] + j <= i:
+                if schedule_list[j][1] + dp[j] > max_num:
+                    max_num = schedule_list[j][1]+dp[j]
+
+    dp[i] = max_num
+
+sys.stdout.write(str(max(dp)))
