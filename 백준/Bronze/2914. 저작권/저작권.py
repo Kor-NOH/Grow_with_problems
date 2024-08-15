9de5ae88842ec2153149232ecaa92b82a536ba84
Noh dongwon <102791306+Kor-NOH@users.noreply.github.com>
2024-08-15 16:30:52 +0900

    [Bronze III] Title: 저작권, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2914. 저작권/저작권.py
+++ 백준/Bronze/2914. 저작권/저작권.py
@@ -2,5 +2,4 @@ import sys
 import math
 
 song_cnt, avg = map(int, sys.stdin.readline().split())
-avg = avg - 1 + 0.01
-sys.stdout.write(str(math.ceil(avg * song_cnt)))
+sys.stdout.write(str(math.ceil((avg-1+0.01) * song_cnt)))
