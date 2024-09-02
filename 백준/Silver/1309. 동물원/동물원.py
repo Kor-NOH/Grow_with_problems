5a297a31b8c4ab51ba05df0ae7c520f162ce877f
Noh dongwon <102791306+Kor-NOH@users.noreply.github.com>
2024-09-02 17:34:14 +0900

    [Silver I] Title: 동물원, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,9 +1,9 @@
 import sys
 
 z_size = int(sys.stdin.readline().strip())
-dp = [[] for _ in range(z_size)]
-dp[0] = [1, 1, 1]
-for i in range(1, z_size):
-    dp[i] = [sum(dp[i-1])%9901,(dp[i-1][0]+dp[i-1][2])%9901,(dp[i-1][0]+dp[i-1][1])%9901]
+a, b, c = 1, 1, 1
 
-sys.stdout.write(str(sum(dp[-1])%9901))
+for i in range(1, z_size):
+    a, b, c = (a + b + c) % 9901, (a + c) % 9901, (a + b) % 9901
+
+sys.stdout.write(str((a + b + c) % 9901))
