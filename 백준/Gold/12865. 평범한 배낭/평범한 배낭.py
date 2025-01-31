64c6e40da1235f0541028fb0078b57b21dae5544
Noh dongwon <102791306+Kor-NOH@users.noreply.github.com>
2025-01-31 12:44:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 2468 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,16 +2,10 @@ import sys
 
 things_cnt, backpack = map(int, sys.stdin.readline().split())
 things_list = [list(map(int, sys.stdin.readline().split())) for _ in range(things_cnt)]
-dp = [[0] * (backpack+1) for _ in range(things_cnt+1)]
+dp = [0] * (backpack + 1)
 
-for i in range(things_cnt):
-    for j in range(backpack+1):
-        if j >= things_list[i][0]:
-            dp[i+1][j] = max(things_list[i][1]+dp[i][j-things_list[i][0]], dp[i][j])
-        else:
-            dp[i+1][j] = dp[i][j]
+for w, v in things_list:
+    for j in range(backpack, w - 1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
 
-if things_cnt == 0:
-    sys.stdout.write('0')
-else:
-    sys.stdout.write(str(dp[-1][-1]))
+sys.stdout.write(str(dp[backpack]))
